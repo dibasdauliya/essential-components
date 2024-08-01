b56ef3278a0471a5cf5cd1ce4cdb5effd6d95d1b
dibasdauliya <dibasdauliya@gmail.com>
2024-07-31 23:23:16 -0400

    add type declaration

---
+++ main.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'emoji-hover-wrapper': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { icon?: string; tag?: string };
+    }
+  }
