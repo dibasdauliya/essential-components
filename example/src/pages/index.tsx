f79c7f1c49370bc8565aa84f9451e38f2719d7bb
dibasdauliya <dibasdauliya@gmail.com>
2024-08-18 23:29:16 -0400

    update docs

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -2,15 +2,14 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
+import React from 'react';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primarry', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -35,9 +34,6 @@ export default function Home(): JSX.Element {
       title={`${siteConfig.title} | ${siteConfig.tagline}`}
       description="Web Components made easy">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
