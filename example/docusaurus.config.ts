5d98b5915acb939ac88bdee9f1e7c167ac53a36d
dibasdauliya <dibasdauliya@gmail.com>
2024-08-25 22:33:34 -0400

    update docs

--- example/docusaurus.config.ts
+++ example/docusaurus.config.ts
@@ -129,7 +129,7 @@ const config: Config = {
         //   ],
         // },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Essential Components. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Essential Components.`,
     },
     prism: {
       theme: prismThemes.github,
