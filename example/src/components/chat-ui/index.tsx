0730a17019f18d0ebdafc0604b9d5c8aa139dfd2
dibasdauliya <dibasdauliya@gmail.com>
2025-07-09 18:21:21 -0400

    add chat UI

---
+++ example/src/components/chat-ui/index.tsx
@@ -0,0 +1,27 @@
+// import React from "react";
+// import BrowserOnly from "@docusaurus/BrowserOnly";
+
+// export function ChatUI({
+//   emoji,
+//   content,
+//   tag,
+// }: {
+//   emoji: string;
+//   content: string;
+//   tag: string;
+// }) {
+//   return (
+//     <BrowserOnly>
+//       {() => {
+//         // Import the custom element only in the browser environment
+//         import("essential-components/src/chat-ui");
+//         return (
+//           <chat-ui
+//     title="Support Chat"
+//     placeholder="How can we help you?">
+// </chat-ui>
+//         );
+//       }}
+//     </BrowserOnly>
+//   );
+// }
