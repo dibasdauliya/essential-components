a8eb2afc7f516b1c1d2172a1cb5cded6a41f0859
dibasdauliya <dibasdauliya@gmail.com>
2025-10-18 19:05:10 -0400

    fix paging issues

--- essential-components/vite.config.ts
+++ essential-components/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/py-code-editor/",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
