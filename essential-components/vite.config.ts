4f115026a2d8d640faabdbd644791152d37a5179
dibasdauliya <dibasdauliya@gmail.com>
2025-10-18 19:07:04 -0400

    fix route isue

--- essential-components/vite.config.ts
+++ essential-components/vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        activitiesWidget: resolve(__dirname, "activities-widget.html"),
+        pyCodeEditor: resolve(__dirname, "py-code-editor.html"),
       },
     },
   },
